bc8835d7abf2dc4ee5d7ad5940d637279aa95f51
Brightlaz <lazbright1@gmail.com>
2022-05-23 17:06:56 -0700

    UX
    sliding effect in-between pages

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,11 +3,19 @@
  * 
  */
 // Scroll variables
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+let counter = 1
+let size = slides[0].clientWidth
+
 const scrollDown = document.querySelector('#scrollDown');
-const overview = document.querySelector('nav')
-    // const info = document.querySelector('#info');
-const homeLink = document.querySelector('#home')
-const home = document.querySelector('.container')
+const overview = document.querySelector('.container2');
+
+const nav = document.querySelector('nav');
+const info = document.querySelector('#info');
+const body = document.querySelector('#body');
+const homeLink = document.querySelector('#home');
+const home = document.querySelector('.container');
 
 // Type variables
 const typingText = document.querySelector('#typing-text');
@@ -17,6 +25,17 @@ const typingDelay = 200;
 const loadingDelay = 2000;
 let dataIndex = 0;
 let charIndex = 0;
+
+
+
+// slider.style.transform = 'translateX(' + (size * counter) + ' px)';
+// info.addEventListener('click', () => {
+//     slider.style.transition = 'transform 0.4s ease-in-out';
+//     counter++;
+//     slider.style.transform = 'translateX(' + (size * counter) + 'px)';
+// })
+
+
 
 
 
@@ -41,14 +60,25 @@ function type() {
 }
 
 window.addEventListener('DOMContentLoaded', () => {
+    fixedNav();
     setTimeout(type, loadingDelay)
+
 });
 
 scrollDown.addEventListener('click', function() {
-    scroll(overview);
+    overview.classList.remove('off');
+    scroll(nav);
+    setTimeout(() => { hidden(home) }, 500)
+
 })
 homeLink.addEventListener('click', function() {
-    scroll(home);
+    home.classList.remove('off')
+    scroll(home)
+    setTimeout(() => { hidden(overview) }, 500)
+    if (window.scrollY === home.offsetTop) {
+        nav.classList.remove('nav_fixed')
+    }
+
     typingText.innerHTML = '';
     dataIndex = 0;
     charIndex = 0;
@@ -62,4 +92,21 @@ function scroll(where) {
         top: where.offsetTop,
         behavior: 'smooth'
     })
+}
+
+function fixedNav() {
+    if (window.scrollY == home.offsetTop) {
+        if (!home.classList.contains('off'))
+            nav.classList.remove('nav_fixed')
+    } else {
+        nav.classList.add('nav_fixed')
+    }
+}
+window.addEventListener('scroll', function() {
+    fixedNav();
+})
+
+function hidden(container1) {
+    container1.classList.add('off')
+
 }
